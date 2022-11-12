85d3de6bea73b1c6f5d77b2ff20dfa212d8cd7b8
Daniel Williams <williamsoligodaniel@gmail.com>
2022-11-12 06:24:18 +0100

    Update Jwt.ts

--- backend/src/auth/Jwt.ts
+++ backend/src/auth/Jwt.ts
@@ -2,9 +2,9 @@ import { JwtPayload } from './JwtPayload'
 import { JwtHeader } from 'jsonwebtoken'
 
 /**
- *  * Interface representing a JWT token
- *   */
+ * Interface representing a JWT token
+ */
 export interface Jwt {
-	  header: JwtHeader
-	    payload: JwtPayload
+  header: JwtHeader
+  payload: JwtPayload
 }
